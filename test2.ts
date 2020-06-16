19acfe4ca4fae0f58f06bb18141f9088fad7805b
Dmitriy Tatarintsev <tatarintsevd@gmail.com>
2020-06-16 23:45:12 +0400

    update test

--- test2.ts
+++ test2.ts
@@ -16,25 +16,29 @@ class Task extends Item implements ITask {
   get created() {
     return this.createdAt ? new Date(this.createdAt) : null;
   }
+
+  toggle() {
+    this.isDone = !this.isDone;
+  }
 }
 
 const TaskCollection = new Collection<ITask, Task>(Task);
 
-const task = await TaskCollection.create({ title: "title" });
-
+const task = await TaskCollection.create({ title: 'improve file structure' });
 await TaskCollection.save(task);
-
 console.log(`created`, task);
 
 const taskOne = await TaskCollection.findById(task.id);
-
-if (taskOne) {
-  console.log(`founded`, taskOne, taskOne.upper, taskOne.created);
-} else {
+if (!taskOne) {
   console.log(`not found`);
   Deno.exit();
 }
 
-const removeCount = await TaskCollection.remove(taskOne);
+console.log(`found`, taskOne, taskOne.upper, taskOne.created);
 
+taskOne.toggle();
+await TaskCollection.save(taskOne);
+console.log(`edited`, taskOne, taskOne.upper, taskOne.created);
+
+const removeCount = await TaskCollection.remove(taskOne);
 console.log(`removed: ${removeCount}`);
